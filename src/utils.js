5a4134cae2f610d866f73319a0ccf3febe63a8d2
zzzgit <zzzgoo@gmail.com>
2025-11-29 14:19:07 +0800

    feat: add detectOS

--- src/utils.js
+++ src/utils.js
@@ -132,7 +132,34 @@ const checkLanguage = (word)=> {
 	return 'other'
 }
 
+const detectOS = ()=> {
+	const userAgent = window.navigator.userAgent
+	const platform = window.navigator?.userAgentData?.platform || window.navigator.platform
+
+	const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K', 'macOS']
+	const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE']
+	const iosPlatforms = ['iPhone', 'iPad', 'iPod']
+
+	let os
+
+	if (macosPlatforms.includes(platform)){
+		os = 'Mac OS'
+	} else if (iosPlatforms.includes(platform)){
+		os = 'iOS'
+	} else if (windowsPlatforms.includes(platform)){
+		os = 'Windows'
+	} else if ((/Android/).test(userAgent)){
+		os = 'Android'
+	} else if ((/Linux/).test(platform)){
+		os = 'Linux'
+	} else {
+		os = 'Unknown'
+	}
+
+	return os
+}
+
 export {
-	generateUUID, getCurrentTab, playSound, getSettings, saveSettings, fnv1aHash, performFetch, checkLanguage, canonLangs, free2canonLang, canon2freeLang,
+	generateUUID, getCurrentTab, playSound, getSettings, saveSettings, fnv1aHash, performFetch, checkLanguage, canonLangs, free2canonLang, canon2freeLang, detectOS,
 }
 
